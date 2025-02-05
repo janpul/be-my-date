36e67586d2dc73e439a34a80d51361851f3a8157
janpul <76933383+janpul@users.noreply.github.com>
2025-02-05 17:06:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,10 +16,15 @@ let messageIndex = 0;
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
+    const gifImage = document.querySelector('.gif_container img');
+
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
     yesButton.style.fontSize = `${currentSize * 1.5}px`;
+
+    // Change the GIF image source
+    gifImage.src = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExODVpeDY0eWFpcnIwd2EwODZwbmhqdjNheHF1MnR2and4YXFheDMxeiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/QlQdLBS70XJcZY1fLF/giphy.gif"; // Replace with the new GIF URL
 }
 
 function handleYesClick() {
